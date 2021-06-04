3cd45908f34f353c64c9011e43dd51e8c4b7d9e3
Coline Dusserre <coline.dusserre@kedgebs.com>
2021-06-04 11:51:10 +0200

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'bookmarks/new'
   get 'bookmarks/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: 'lists#index'
 resources :lists, only: [:index, :show, :create, :new] do
   resources :bookmarks, only: [:create, :new]
 end
